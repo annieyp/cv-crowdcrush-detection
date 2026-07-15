cb2ac9cd91c8567b5f8c44c53067a60638f3f3f4
annieyp <annie.yeonwon.park@gmail.com>
2026-07-14 21:39:24 -0400

    csrnet architecture implemented following section 3.1.2 in paper (Network Architecture)

---
+++ src/csrnet.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+
+
+class CSRNet(nn.Module):
+    def __init__(self):
+        super(CSRNet, self).__init__()
+
+        #frontend
+        self.front1 = nn.Sequential(
+            nn.Conv2d(3, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.front2 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.front3 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.front4 = nn.Sequential(
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+        #backend
+        self.backend = nn.Sequential(
+            nn.Conv2d(512, 512, 3, padding=2, dilation=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=2, dilation=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=2, dilation=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 256, 3, padding=2, dilation=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 128, 3, padding=2, dilation=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 64, 3, padding=2, dilation=2),
+            nn.ReLU(inplace=True)
+        )
+
+        self.outputLayer = nn.Conv2d(64, 1, kernel_size=1)
+
+    def forward(self, x):
+        x = self.front1(x)
+        x = self.front2(x)
+        x = self.front3(x)
+        x = self.front4(x)
+        x = self.backend(x)
+        x = self.outputLayer(x)
+
+        return x
