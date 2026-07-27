64e4be88a344deaafe44ee77b7638440be6777a0
annieyp <annie.yeonwon.park@gmail.com>
2026-07-27 13:32:46 -0400

    Added gradient clipping to remedy gradient explosion

--- src/train.py
+++ src/train.py
@@ -14,6 +14,7 @@ def train(dataloader, model, loss_fn, optimizer, device):
         loss = loss_fn(pred, y)
 
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
         optimizer.zero_grad()
 
